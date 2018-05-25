8dfcd025a7c36ac40c74734b9426fc2a9fa0179d
keomas <keomas.santos@gmail.com>
2018-05-25 12:20:46 -0300

    busca isbn voltando ao google 1

--- main.js
+++ main.js
@@ -1,49 +1,56 @@
 
-$("button").click(function () {
+function search () {
  
-var div =	$(".hidden").show();
+$("#wait").modal({
+	backdrop: 'static'
+});
 
     var	isbnEntry = $("#nisbn").val() ;
     isbn_array=isbnEntry.split(",").map(item => item.trim());;
-   	//var urlBook = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
-  	var urlBookHead = "http://xisbn.worldcat.org/webservices/xid/isbn/";
-  	var urlBookTail = "%20?method=getEditions&fl=*&format=json";
+var urlBook = "https://www.googleapis.com/books/v1/volumes?q=isbn:";
+  //	var urlBookHead = "http://xisbn.worldcat.org/webservices/xid/isbn/";
+  //	var urlBookTail = "%20?method=getEditions&fl=*&format=json";
   	console.log(isbn_array.length);
   	
   	for (i = 0; i < isbn_array.length; i++){
 		 var aux= isbn_array[i];
-  		var y =urlBookHead+ aux+urlBookTail;
+  		var y =urlBook+ aux;
   		$.ajax({
             async:false,
             url:y,
-            dataType:"json",
+			dataType:"json",
+			headers:{'Access-Control-Allow-Origin':'*'},
             success:function(data){
-            //console.log(data.list[0]);
-           	if(data.list != undefined){
+            console.log(data);
+           	if(data.items != undefined){
 
 
-		           	var book = data.list[0];
-		           	var title = (book["title"]);
-			        var author = (book["author"]);
-			        var publisher = (book["publisher"]);
-			        var publishedDate = (book["year"]);
+				var book = data.items[0];
+	       
+				var title = (book["volumeInfo"]["title"]);
+				
+				var authors = (book["volumeInfo"]["authors"]).join(", ");
+				
+				var publisher = (book["volumeInfo"]["publisher"]);
+				var publishedDate = (book["volumeInfo"]["publishedDate"]);
+				
 			         $( "<tr>"+
 			                    '<th scope="row">'+aux+"</th>"+
 			                    "<td>"+title+"</td>"+
-			                    "<td>"+author+"</td>"+
+			                    "<td>"+authors+"</td>"+
 			                    "<td>"+publisher+"</td>"+
 			                    "<td>"+publishedDate+"</td>"+
 			                "</tr>"
 			                ).appendTo( "tbody" );
 			}else{
 					var title = "livro não encontrado pela api ";
-	                var author =  "não encontrado";
+	                var authors =  "não encontrado";
 	                var publisher = "não encontrado";	                
 	                var publishedDate ="não encontrado" ;
 	                $( "<tr>"+
 	                    '<th scope="row">'+aux+"</th>"+
 	                    "<td>"+title+"</td>"+
-	                    "<td>"+author+"</td>"+
+	                    "<td>"+authors+"</td>"+
 	                    "<td>"+publisher+"</td>"+
 	                    "<td>"+publishedDate+"</td>"+
 	                "</tr>"
@@ -63,6 +70,7 @@ var div =	$(".hidden").show();
         }
        
 		); 
+		
 		aux="";
          y="";
 
@@ -82,9 +90,12 @@ var div =	$(".hidden").show();
 		exportButtons: true,
 		position:'top', 
 	});
-	div.hide();
+
+	$('#wait').modal("hide");
+	$('#wait').css("display","none");
+	$('#wait').attr("class","modal");
     }
-   );
+   
    
  
 
